514a5d5fe4602903d63db47e868a6c69b8e57474
Ricardo Matos <ricardovrmatos@gmail.com>
2025-02-23 22:54:03 -0300

    Primeiro commit: inicializando o repositório

---
+++ src/main/java/com/rvrm/consultoria_tec/ConsultoriaTecApplication.java
@@ -0,0 +1,13 @@
+package com.rvrm.consultoria_tec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultoriaTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultoriaTecApplication.class, args);
+	}
+
+}
