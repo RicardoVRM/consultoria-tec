cb5f40d5446fb4cdcb5df057bfad1397e17fdd25
Ricardo Matos <ricardovrmatos@gmail.com>
2025-02-23 23:19:08 -0300

    ajuste arquivos

---
+++ src/test/java/com/rvrm/consultoria_tec/ConsultoriaTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvrm.consultoria_tec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultoriaTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
